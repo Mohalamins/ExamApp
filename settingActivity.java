063643b80de2884ef76af076d94a66d025d2389c
Mohalamins <167947375+Mohalamins@users.noreply.github.com>
2024-05-24 00:28:55 +0300

    Add files via upload

---
+++ settingActivity.java
@@ -0,0 +1,19 @@
+package com.mas24.examapp;
+
+
+import static android.os.Build.VERSION_CODES.R;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.mas.examapp.R;
+
+public class settingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R);
+    }
+}
